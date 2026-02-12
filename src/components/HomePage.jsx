b02eafe5481ed705f8b9500bb35031349e2c4131
soumya tiwari <soumyatiwari7866@gmail.com>
2026-02-13 00:53:04 +0530

    feat:add About Us page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -22,6 +22,29 @@ const Login = () => {
     },
   ];
 
+  const news =[
+    {
+      title : "Flood in Maharashtra",
+      description : "Heavy rains have caused severe flooding in Maharashtra, leading to widespread damage and displacement of residents. Rescue operations are underway to assist those affected by the floods."
+    },
+    {
+      title : "Earthquake in Turkey",
+      description : "A powerful earthquake struck Turkey, causing significant destruction and loss of life. Rescue teams are working tirelessly to search for survivors and provide aid to those affected by the earthquake."
+    },
+    {
+      title : "Wildfires in California",
+      description : "Intense wildfires have engulfed parts of California, leading to evacuations and extensive damage to homes and natural habitats. Firefighters are battling the flames to contain the spread and protect communities."
+    },
+    {
+      title : "Cyclone in Bangladesh",
+      description : "A severe cyclone has hit Bangladesh, causing widespread devastation and loss of life. Rescue efforts are underway to provide relief and support to those affected by the cyclone."
+    },
+    {
+      title : "Tsunami in Indonesia",
+      description : "A powerful tsunami has struck Indonesia, resulting in significant damage and loss of life. Rescue teams are working to assist survivors and provide aid to those affected by the tsunami."
+    },
+  ]
+
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => prevIndex === images.length - 1 ? 0 : prevIndex + 1);
   }
@@ -55,6 +78,24 @@ const Login = () => {
           </div>
 
           {/* <p className={Styles.heading}>Preparation through education is less costly than learning through tragedy.</p> */}
+        </div>
+        <div className={Styles.about}>
+          <h2>About us</h2>
+          <p>Unawareness and ignorance can cost life</p>
+          <p>Let's prepare ourself for the upcoming disaster , so that no lives be gone becaues of unawareness and ignoring the importance of disaster preparedness .</p>
+
+          <div className={Styles.newsSection}>
+            {news.map((el,idx)=>{
+            return(
+            // <>
+            <div className={Styles.news} key={idx}> 
+              <div>{el.title}</div>
+              <div>{el.description}</div>
+            </div>
+            // </>
+            )
+          })}
+          </div>
         </div>
       </div>
     </>
