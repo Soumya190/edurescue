bbf8021fdf831d6b8407fa36cabb791e55ef14b6
soumya tiwari <soumyatiwari7866@gmail.com>
2026-02-14 21:13:06 +0530

    fix:prevent news cards from shrinking and enable horizontal overflow

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import Styles from '@/Styles/HomePage/homepage.module.scss'
 
 const Login = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [newsData, setNewsData] = useState([]);
 
   const images = [
     {
@@ -22,32 +23,44 @@ const Login = () => {
     },
   ];
 
-  const news =[
+  const news = [
     {
-      title : "Flood in Maharashtra",
-      description : "Heavy rains have caused severe flooding in Maharashtra, leading to widespread damage and displacement of residents. Rescue operations are underway to assist those affected by the floods."
+      title: "Flood in Maharashtra",
+      description: "Heavy rains have caused severe flooding in Maharashtra, leading to widespread damage and displacement of residents. Rescue operations are underway to assist those affected by the floods."
     },
     {
-      title : "Earthquake in Turkey",
-      description : "A powerful earthquake struck Turkey, causing significant destruction and loss of life. Rescue teams are working tirelessly to search for survivors and provide aid to those affected by the earthquake."
+      title: "Earthquake in Turkey",
+      description: "A powerful earthquake struck Turkey, causing significant destruction and loss of life. Rescue teams are working tirelessly to search for survivors and provide aid to those affected by the earthquake."
     },
     {
-      title : "Wildfires in California",
-      description : "Intense wildfires have engulfed parts of California, leading to evacuations and extensive damage to homes and natural habitats. Firefighters are battling the flames to contain the spread and protect communities."
+      title: "Wildfires in California",
+      description: "Intense wildfires have engulfed parts of California, leading to evacuations and extensive damage to homes and natural habitats. Firefighters are battling the flames to contain the spread and protect communities."
     },
     {
-      title : "Cyclone in Bangladesh",
-      description : "A severe cyclone has hit Bangladesh, causing widespread devastation and loss of life. Rescue efforts are underway to provide relief and support to those affected by the cyclone."
+      title: "Cyclone in Bangladesh",
+      description: "A severe cyclone has hit Bangladesh, causing widespread devastation and loss of life. Rescue efforts are underway to provide relief and support to those affected by the cyclone."
     },
-    {
-      title : "Tsunami in Indonesia",
-      description : "A powerful tsunami has struck Indonesia, resulting in significant damage and loss of life. Rescue teams are working to assist survivors and provide aid to those affected by the tsunami."
-    },
+    // {
+    //   title : "Tsunami in Indonesia",
+    //   description : "A powerful tsunami has struck Indonesia, resulting in significant damage and loss of life. Rescue teams are working to assist survivors and provide aid to those affected by the tsunami."
+    // },
   ]
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => prevIndex === images.length - 1 ? 0 : prevIndex + 1);
   }
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      let data = await fetch("https://newsdata.io/api/1/latest?apikey=pub_3ff884d506af452b8feecd9368c91f84&q=fire&country=in");
+      console.log(data);
+      let res = await data.json();
+      console.log(res);
+      setNewsData(res.results);
+    }
+
+    fetchNews();
+  }, []);
 
 
   // useEffect(() => {
@@ -55,6 +68,7 @@ const Login = () => {
   //   return () => clearInterval(interval);
 
   // }, [currentIndex])
+  // console.log(newsData.language);
 
   return (
     <>
@@ -82,19 +96,20 @@ const Login = () => {
         <div className={Styles.about}>
           <h2>About us</h2>
           <p>Unawareness and ignorance can cost life</p>
-          <p>Let's prepare ourself for the upcoming disaster , so that no lives be gone becaues of unawareness and ignoring the importance of disaster preparedness .</p>
+          {/* <p>Let's prepare ourself for the upcoming disaster , so that no lives be gone becaues of unawareness and ignoring the importance of disaster preparedness .</p> */}
 
           <div className={Styles.newsSection}>
-            {news.map((el,idx)=>{
-            return(
-            // <>
-            <div className={Styles.news} key={idx}> 
-              <div>{el.title}</div>
-              <div>{el.description}</div>
-            </div>
-            // </>
-            )
-          })}
+            {news.map((el, idx) => {
+              return (
+                // <>
+                <div className={Styles.news} key={idx}>
+                  <img src={newsData[idx].image_url} alt="" className={Styles.newsImages} />
+                  <div>{el.title}</div>
+                  <div>{el.description}</div>
+                </div>
+                // </>
+              )
+            })}
           </div>
         </div>
       </div>
